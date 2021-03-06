e0573e9c6a78b833e35f1962304a56f700c1a1dc
Eunhee-JJ <eunhee0324@g.skku.edu>
2021-03-06 22:25:35 +0900

    (3장) 위젯으로 값 전달 MeterialApp으로 표현

--- hello_world/lib/Person.dart
+++ hello_world/lib/Person.dart
@@ -5,6 +5,20 @@ class Person extends StatelessWidget {
   final String lastName;
   Person({this.firstName, this.lastName});
   Widget build(BuildContext context) {
-    return Container(child: Text('$firstName $lastName'));
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("A fancier app"),
+        ),
+        body: Container(
+          alignment: Alignment.center,
+          child: Text('$firstName $lastName'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.thumb_up),
+          onPressed: () => {},
+        ),
+      ),
+    );
   }
 }
