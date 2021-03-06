b22c6d9f9ed4c78ea9ec41d85b67921c1470a782
Eunhee-JJ <eunhee0324@g.skku.edu>
2021-03-06 21:49:21 +0900

    3장- 위젯이 전부다: 상태가 없는 위젯을 만드는 방법

---
+++ hello_world/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(RootWidget());
+
+class RootWidget extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Text("Hello world");
+  }
+}
+
